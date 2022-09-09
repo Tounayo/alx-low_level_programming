1a59bb75e647ec69414e053752da8cf2c3a3f719
Tounayo <tounayoobogbeni@yahoo.com>
2022-09-08 18:15:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - prints "\"Programming is like building a multilingual puzzle"
 
- * main - Entry point
-
- * Description: prints "\"Programming is like building a multilingual puzzle"
-
- * Return: 0
+ * Return: Always 0
 
  */
-
 int main(void)
-  
-{
-  
+  {
   puts("\"Programming is like building a multilingual puzzle");
-  
   return (0);
-  
 }
