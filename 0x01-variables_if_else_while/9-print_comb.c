53c3dfa78d94c575da02f403b0ae19cb84a1a1ba
Tounayo <tounayoobogbeni@yahoo.com>
2022-09-12 09:53:23 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,15 @@
 * main - Entry point
 * Return: Always 0 (Success)
 */
-int main(void)  
+int main(void)
 {
 int d;
 for (d = '0'; d <= '9'; d++)
 {
 putchar(d);
-if (d != '9')	
-{	  
-putchar(',');	  
+if (d != '9')
+{
+putchar(',');
 putchar(' ');
 }
 }
