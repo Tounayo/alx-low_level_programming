320cf0cbee8a76b9433263c60d9ad1124d6b536e
Tounayo <tounayoobogbeni@yahoo.com>
2022-09-12 14:09:43 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-int i, j, k, l;  
+int i, j, k, l;
 for (i = 0; i < 1000; i++)
 {
-j = i / 100;      
+j = i / 100;
 k = (i / 10) % 10;
 l = i % 10;
 if (j < k && k < l)
@@ -19,13 +19,13 @@ if (j < k && k < l)
 putchar(j + '0');
 putchar(k + '0');
 putchar(l + '0');
-if (i < 700)	    
+if (i < 700)
 {
 putchar(44);
-putchar(32);    
+putchar(32);
 }
 }
 }
-putchar('\n');  
+putchar('\n');
 return (0);
 }
