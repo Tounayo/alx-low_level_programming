2dd9e9156bc026b6bf6e33677df0a9a862e0ee8e
Tounayo <tounayoobogbeni@yahoo.com>
2022-09-12 08:02:08 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low != e && low !=q)
+{
+if (low != e && low !=q
+putchar(low);
+}
+printf("\n");
+returm (0);
+}
