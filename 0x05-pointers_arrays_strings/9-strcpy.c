3682684ea24d7f2374eb50422e0b86519902817a
Tounayo <tounayoobogbeni@yahoo.com>
2022-09-19 16:17:38 -0700

    all_task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 
 /**
  * _strcpy - copies the string pointed to by src
- * including the termination null byte, to the 
+ * including the termination null byte, to the
  * buffer pointed to by dest
  * @dest: destination
  * @src: source
