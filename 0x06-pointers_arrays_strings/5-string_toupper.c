34263f78d2533e07d86b8b07f57cfe646b61411c
Tounayo <tounayoobogbeni@yahoo.com>
2022-09-23 08:34:54 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,10 +2,10 @@
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
+ *
  * @s: input string.
  * Return: the pointer to dest.
  */
-
 char *string_toupper(char *s)
 {
 int count = 0;
